a2fae3998f6ecc3c945f726042f589ae3b7d6510
JordanYanakiev <jordan.yanakiev@gmail.com>
2018-02-12 15:52:45 +0200

    Array Example

---
+++ Array/Array/Array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string[] capitals = { "Malo konare", "Golyamo konare" };
+
+            foreach (string city in capitals)
+            {
+                Console.WriteLine(city);
+            }
+
+            
+        }
+
+    }
+}
+
