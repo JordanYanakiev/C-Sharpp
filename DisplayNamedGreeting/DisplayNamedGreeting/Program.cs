2e0372c38d44256face4dd051700d00b20b5925c
JordanYanakiev <jordan.yanakiev@gmail.com>
2018-02-14 17:37:39 +0200

    Method_DisplayNamedGreeting

---
+++ DisplayNamedGreeting/DisplayNamedGreeting/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Напишете метод, който при подадено име отпечатва в конзолата
+"Hello, <name>!" (например "Hello, Peter!") */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayNamedGreeting
+{
+    class Program
+    {
+        //The method which will display the greeting
+        static void DisplayGreeting(string name)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Hello, {0}!", name);
+            Console.WriteLine();
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Please, enter your name: ");
+            string name = Console.ReadLine();
+            DisplayGreeting(name);
+        }
+    }
+}
