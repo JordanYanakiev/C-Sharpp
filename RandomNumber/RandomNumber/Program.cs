b141a6dd1296a8b42af539b8783d9d36601f26e7
JordanYanakiev <jordan.yanakiev@gmail.com>
2018-04-20 21:29:35 +0300

    Random Number Generate

---
+++ RandomNumber/RandomNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿/* 
+ Write a program which prints a random number in the range from 100 to 200.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random number = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(" " + number.Next(100, 200) + " ");
+            }
+            
+        }
+    }
+}
