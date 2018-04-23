8ded40a61c1aeefedfdd01bdeca42e22d852126c
JordanYanakiev <jordan.yanakiev@gmail.com>
2018-04-23 12:34:53 +0300

    Hypotenuse Calculation

---
+++ HypotenuseCalculation/HypotenuseCalculation/Program.cs
@@ -0,0 +1,29 @@
+﻿/* 
+ Write a program which calculates the vlue of 
+ the hypotenuse by given two kattes. 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HypotenuseCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize and enter the values of the kattes
+            Console.Write("Katte 1 = ");
+            double katte1 = double.Parse(Console.ReadLine());
+            Console.Write("Katte 2 = ");
+            double katte2 = double.Parse(Console.ReadLine());
+
+            // Initialize the hypotenuse and calculating its value
+            double hypotenuse = Math.Sqrt(Math.Pow(katte1, 2) + Math.Pow(katte2, 2));
+            Console.WriteLine("Hypotenuse = {0}", Math.Round(hypotenuse, 2));  // Displays the value of the hypotenuse with precison set to two symbols after the comma
+        }
+    }
+}
