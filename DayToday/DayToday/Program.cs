c9b085f471b35dc9997ee66109015b4a91f32e9f
JordanYanakiev <jordan.yanakiev@gmail.com>
2018-04-23 10:42:56 +0300

    Day Today

---
+++ DayToday/DayToday/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 
+ Write a program which displays on the console
+ the curent day of the week.  
+  */
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayToday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine (DateTime.Now.DayOfWeek); 
+        }
+    }
+}
