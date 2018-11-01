36ab3ebb515b2d94055e8f19771dfd1007c62f02
JordanYanakiev <jordan.yanakiev@gmail.com>
2018-11-01 12:27:26 +0200

    DisplayUnicode
    Displayes the Unicode encoding of  a string entered from the console.

---
+++ ConvertToUnicode/ConvertToUnicode/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Write a program which extracts the unucode 
+ * code of the characters of a word entered from the console.  */
+
+namespace ConvertToUnicode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word to check its' Unicode encoding: ");
+            string word = Console.ReadLine();
+            char[] symb = new char[word.Length];
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                symb[i] = word[i];
+
+                Console.Write("u{0:x4} \\ ", Convert.ToUInt16(symb[i]));
+            }
+            Console.WriteLine();
+        }
+    }
+}
