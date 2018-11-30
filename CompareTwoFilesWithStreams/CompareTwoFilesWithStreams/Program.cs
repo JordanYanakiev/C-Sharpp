c5ec24da765a09149a5410a35668fbf7857702d7
JordanYanakiev <jordan.yanakiev@gmail.com>
2018-11-30 13:59:06 +0200

    Compares two files and show the number of same and different rows

---
+++ CompareTwoFilesWithStreams/CompareTwoFilesWithStreams/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+/* Write a program which compares two files 
+ * and prints the number of equal rows and 
+ * the number of different rows */
+
+
+namespace CompareTwoFilesWithStreams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create stream readers for file 1 and 2
+                Encoding encode = Encoding.GetEncoding("UTF-8");
+                StreamReader read1 = new StreamReader("file1.txt", encode);
+                StreamReader read2 = new StreamReader("file2.txt", encode);
+
+                string line1 = read1.ReadLine();
+                string line2 = read2.ReadLine();
+
+            //Create counters for same and diferent rows
+                int countDuplicate = 0;
+                int countDifferent = 0;
+
+
+                while (line1 != null || line2 != null)
+                {
+
+                    //If the files are not equally long throw exception that says the files are with diferent lenght
+                    if ((line1 != null && line2 == null) || (line1 == null && line2 != null))
+                    {
+                        throw new Exception("One of both files is longer! Please, make sure the files are equally long!");
+                    }
+
+                    //Compare the lines and update the counters accordingly
+                    if (line1 == line2)
+                    {
+                        countDuplicate++;
+                    }
+                    else
+                        countDifferent++;
+
+                    line1 = read1.ReadLine();
+                    line2 = read2.ReadLine();
+                }
+
+
+                //Show the result and clse the streams
+                Console.WriteLine("The rows that mach are {0} and the rows that don't are {1}", countDuplicate, countDifferent);
+
+                read1.Close();
+                read2.Close();
+            
+
+
+        }
+    }
+}
