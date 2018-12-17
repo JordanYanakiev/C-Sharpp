ca3c8ecca43b81a03dd206b6486e497a7c8c6c91
JordanYanakiev <jordan.yanakiev@gmail.com>
2018-12-17 12:44:58 +0200

    Delete negative numbers from array and display the positive  List<T>

---
+++ DeleteNegativeNumbers/DeleteNegativeNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Write a program which deletes the negative 
+ * numbers from an entry array. For example  
+ * array = {19, -10, 12, -6, -3, 34, -2, 5}  {19, 12, 34, 5}
+ */
+
+
+namespace DeleteNegativeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 19, -10, 12, -6, -3, 34, -2, 5 }; 
+            List<int> positiveNumbers = new List<int>();
+            int positiveNumberLength = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(array[i] > 0)
+                {
+                    positiveNumbers.Add(array[i]); //Adds the positive numberrs in the List
+                }
+            }
+
+            //Goes through the list to read the numbers stored there
+            positiveNumberLength = positiveNumbers.Count;
+            for (int j = 0; j < positiveNumberLength; j++)
+            {
+                Console.WriteLine(positiveNumbers[j]);
+            }
+
+
+        }
+    }
+}
